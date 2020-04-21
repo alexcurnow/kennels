eb689c46d560ea3b1417078ffb2a3ee1b362cbd2
Alex Curnow <alexcurnow@gmail.com>
2020-04-21 11:54:01 -0500

    updated animals and employees to show their respective customer and location ids.

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -4,5 +4,11 @@ export default (props) => (
   <section className="animal">
     <h3 className="animal__name">Name: {props.animal.name}</h3>
     <div className="animal__breed">Breed: {props.animal.breed}</div>
+    <div className="animal__location">
+      LocationId: {props.animal.locationId}
+    </div>
+    <div className="animal__customer">
+      CustomerId: {props.animal.customerId}
+    </div>
   </section>
 )
--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -5,6 +5,9 @@ export default (props) => (
     <section className="employee">
       <h3 className="employee__name">{props.employee.name}</h3>
       <div className="employee__address">{props.employee.address}</div>
+      <div className="employee__location">
+        LocationId: {props.employee.locationId}
+      </div>
     </section>
   </>
 )
