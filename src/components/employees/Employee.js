626c86dd65a05561b63cb8a32dc9ea18b39a5426
Alex Curnow <alexcurnow@gmail.com>
2020-04-22 10:51:43 -0500

    added another employee

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -4,7 +4,7 @@ export default ({ employee, location }) => (
   <>
     <section className="employee">
       <h3 className="employee__name">{employee.name}</h3>
-      <div className="employee__address">{employee.address}</div>
+      <div className="employee__address">Address: {employee.address}</div>
       <div className="employee__location">Location: {location.name}</div>
     </section>
   </>
