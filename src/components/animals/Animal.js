c8569ea2d043b253c0b6bba903d2e6f4b2a3cb60
Alex Curnow <alexcurnow@gmail.com>
2020-04-27 14:56:24 -0500

    Deleted a stray, unused import.

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export const Animal = ({ animal, location, customer }) => (
   <section className="animal">
-    <h3 className="animal__name">Name: {animal.name}</h3>
     <div className="animal__breed">Breed: {animal.breed}</div>
     <div className="animal__location">Location: {location.name}</div>
     <div className="animal__customer">Customer: {customer.name}</div>
